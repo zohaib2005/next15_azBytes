89042fb74980d9ba023ccd0d5a0ec69342c42df3
zohaib <zohaibshafiq2005@gmail.com>
2025-07-16 18:41:10 +0500

    delete user by id

--- src/app/users/api/[id]/route.ts
+++ src/app/users/api/[id]/route.ts
@@ -8,3 +8,26 @@ export async function GET(
   const user = users.find((item) => item.id == Number(id));
   return Response.json(user);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const userRequest = await request.json();
+  const userIndex = users.findIndex((item) => item.id == Number(id));
+  users[userIndex]["name"] = userRequest.name;
+  return Response.json(users[userIndex]);
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const userIndex = users.findIndex((item) => item.id == Number(id));
+  if (userIndex > 0) {
+    users.splice(userIndex, 1);
+  }
+  return Response.json({ message: "User Deleted" });
+}
