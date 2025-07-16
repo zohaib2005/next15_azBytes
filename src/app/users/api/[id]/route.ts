723044d370690e511eeae92a48cd718e4aba2db2
zohaib <zohaibshafiq2005@gmail.com>
2025-07-16 15:02:47 +0500

    find user by id

--- src/app/users/api/[id]/route.ts
+++ src/app/users/api/[id]/route.ts
@@ -1,3 +1,10 @@
-export async function GET(request: ) {
+import { users } from "@/app/data/users";
 
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const user = users.find((item) => item.id == Number(id));
+  return Response.json(user);
 }
